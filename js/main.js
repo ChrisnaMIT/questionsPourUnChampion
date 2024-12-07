8fd9b62fc2219fb1c0046e286d73d1692962535e
chrisna <chrisna.mitt@gmail.com>
2024-12-07 16:17:19 +0100

    ajoute de reponse incorrect et correct

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const reponseC = document.querySelector('.reponseAChoisirC')
 const reponseD = document.querySelector('.reponseAChoisirD')
 
 
-loadQuizzBouton.addEventListener('click', async  () => { //Ajout d'un evenment click, quand on click sa affiche les questions
+loadQuizzBouton.addEventListener('click',   () => { //Ajout d'un evenment click, quand on click sa affiche les questions
 
     fetch('https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple')// récupération de l'api, on récupere une question
         .then(res => res.json())
@@ -16,30 +16,44 @@ loadQuizzBouton.addEventListener('click', async  () => { //Ajout d'un evenment c
             const question = data.results[0].question;//création de la const question et on mets la data dans la constante question
             const reponseCorrect = data.results[0].correct_answer;
             const reponseIncorrect = data.results[1].incorrect_answers;
-            const reponseIncorrectDeux = data.results(2).incorrect_answers;
-            const reponseIncorrectTroix = data.results(3).incorrect_answers;
+            const reponseIncorrectDeux = data.results[2].incorrect_answers;
+            const reponseIncorrectTroix = data.results[3].incorrect_answers;
 
             console.log(data)
 
             reponseA.innerHTML = reponseCorrect;
             reponseB.innerHTML = reponseIncorrect;
-           // reponseC.innerHTML = reponseIncorrectDeux
-            //reponseD.innerHTML = reponseIncorrectTroix
+            reponseC.innerHTML = reponseIncorrectDeux
+            reponseD.innerHTML = reponseIncorrectTroix
             questionsContainer.innerHTML = question; // dans mon questionContainer, c'est la ou sera ma question
 
         })
         .catch(error => { // catch permet de gérer une erreur avec une api, si il y a une erreur sa affiche ce message
             questionsContainer.innerHTML = "Erreur lors du chargement de la question"; // si on a une erreur dans la question, sa affiche ce message dans le questionContainer
-            reponseA.innerHTML = "Erreur lors du chargement de la question";
-            reponseB.innerHTML = " Erreur lors du chargement de la question";
-           // reponseC.innerHTML = " Erreur lors du chargement de la question";
-           // reponseD.innerHTML = " Erreur lors du chargement de la question";
+            reponseA.innerHTML = "Erreur ";
+            reponseB.innerHTML = " Erreur ";
+            reponseC.innerHTML = " Erreur ";
+            reponseD.innerHTML = " Erreur ";
 
         });
 
 });
 
+function choisirUneReponse(correctAnswer){
 
+    const bouton = document.querySelectorAll('#button')
+
+    bouton.forEach(element => {
+        bouton.addEventListener('click',(event) => {
+            const choix = event.target.innerHTML;
+            if (choix === correctAnswer) {
+                alert('BONNE REPONSE');
+            } else {
+                alert('MAUVAISE REPONSE');
+            }
+        });
+    });
+}
 
 
 
